948e57a5bee96da6226ac5ec5293b6af3631cc21
Mark C. Saur <mcsaur@yahoo.com>
2025-02-19 14:46:03 -0500

    chore: Update dependencies and ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+export default {
+  ignores: [
+    'dist/**',
+    'node_modules/**',
+    'coverage/**',
+    'lib/**',
+    '*.min.js',
+    'bundle.js',
+    '**/vendor/**',
+  ],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+  },
+  languageOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+};
